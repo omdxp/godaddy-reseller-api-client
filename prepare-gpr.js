0d25d4a6fe1d6360235d9cdd9f61fbd40b0340e7
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-10-08 18:35:41 +0100

    fix: not using capital letters in the package name

--- prepare-gpr.js
+++ prepare-gpr.js
@@ -2,6 +2,6 @@ import fs from "fs";
 
 const p = JSON.parse(fs.readFileSync("./package.json", "utf8"));
 
-p.name = `@Omar-Belghaouti/${p.name}`;
+p.name = `@omar-belghaouti/${p.name}`;
 
 fs.writeFileSync("./package.json", JSON.stringify(p, null, 2));
